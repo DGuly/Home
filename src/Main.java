085b752d2467fcfb26fbd5698a0a4f49d579bbaf
dguly <digulUrb@gmail.com>
2015-01-29 02:00:49 +0200

    Reverting good commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,5 @@ public class Main {
         System.out.println("Test");
         System.out.println("Just text");
         System.out.println("BAD BAD COMMIT");
-        System.out.println("Normal changes, dont touch them, thanks");
      }
 }
